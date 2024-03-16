adb018b88050e236cf536490042c783fa65987f8
Lucas Larry <lucasslarryy@gmail.com>
2024-03-15 21:33:58 -0300

    Create

---
+++ principal/script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('indexForm').addEventListener('submit', function (event) {
+        event.preventDefault();
+        window.location.href = 'greeting.html';
+    });
+});
